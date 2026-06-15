5e1910728f666ef76e5f8d36a0e9b4617f06acdf
Samira Hernandez <samyspop2022@gmail.com>
2026-06-14 22:40:07 -0400

    agregandoproductos

--- src/data/llaveros.js
+++ src/data/llaveros.js
@@ -1,6 +1,10 @@
 import luffy from '../assets/llaveros/luffy.jpg';
 import pochacco from '../assets/llaveros/Pochacco.jpg';
-import zorroMediano from '../assets/llaveros/zorro mediano.jpg';  // ← Agrega esta importación
+import zorroMediano from '../assets/llaveros/zorro mediano.jpg';
+import patocongorroderana from '../assets/llaveros/pato con gorro de rana.jpg';
+import llaveroperritopug from '../assets/llaveros/llavero perrito pug.jpg';
+import capibara from '../assets/llaveros/capibara.jpg';
+import pikachu from '../assets/llaveros/pikachu.jpg';
 
 export const llaveros = [
   {
@@ -37,5 +41,53 @@ export const llaveros = [
     stock: 1,
     descripcion: "Llavero de Zorro, tierno y resistente.",
     imagen: zorroMediano, 
+  },
+
+  {
+    id: 4,
+    nombre: "Llavero de Pato con Gorro de Rana",
+    precio: 6,
+    categoria: "Llaveros",
+    material: "Hilo 60% Algodón 40% Acrílico",
+    medida: "5 cm",
+    stock: 1,
+    descripcion: "Llavero de Pato, tierno y resistente.",
+    imagen: patocongorroderana, 
+  },
+
+  {
+    id: 5,
+    nombre: "Mini Llavero de Pug",
+    precio: 1.50,
+    categoria: "Llaveros",
+    material: "Hilo 60% Algodón 40% Acrílico",
+    medida: "3 cm",
+    stock: 1,
+    descripcion: "Llavero de Perro Pug, tierno y resistente.",
+    imagen: llaveroperritopug, 
+  },
+
+  {
+    id: 6,
+    nombre: "Llavero Capibara",
+    precio: 3,
+    categoria: "Llaveros",
+    material: "Hilo 60% Algodón 40% Acrílico",
+    medida: "5 cm",
+    stock: 1,
+    descripcion: "Llavero de Capibara, tierno y resistente.",
+    imagen: capibara, 
+  },
+
+  {
+    id: 7,
+    nombre: "Llavero de Pikachu",
+    precio: 8,
+    categoria: "Llaveros",
+    material: "Hilo 60% Algodón 40% Acrílico",
+    medida: "10 cm",
+    stock: 1,
+    descripcion: "Llavero de Pikachu, tierno y resistente.",
+    imagen: pikachu, 
   },
 ];
